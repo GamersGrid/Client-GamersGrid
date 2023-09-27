bb862bafcbddd04bda4696ed07c46655809ede73
Pedro Figueiredo <pedro.figueiredo35@gmail.com>
2023-09-27 11:18:15 +0100

    add functional tournament page

--- src/components/AddTournament.jsx
+++ src/components/AddTournament.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import axios from "axios";
 import "../App.css";
 import { useNavigate } from "react-router-dom";
@@ -6,13 +6,33 @@ import { useNavigate } from "react-router-dom";
 function AddTournament(props) {
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
+  const [prize, setPrize] = useState("");
+  const [participants, setParticipants] = useState("");
+  const [selectedGame, setSelectedGame] = useState(""); // Store the selected game separately
+  const [games, setGames] = useState([]); // Store the list of games
+  const [dateTime, setDateTime] = useState("");
+
+  useEffect(() => {
+    axios
+      .get(`${import.meta.env.VITE_API_URL}/api/games`)
+      .then((response) => {
+        setGames(response.data);
+      })
+      .catch((error) => console.log(error));
+  }, []);
+
   const navigate = useNavigate();
+
   const handleSubmit = (e) => {
     e.preventDefault();
 
     const requestBody = {
       title,
       description,
+      prize,
+      participants,
+      game: selectedGame, // Use the selectedGame state
+      dateTime,
     };
 
     const storedToken = localStorage.getItem("authToken");
@@ -22,12 +42,14 @@ function AddTournament(props) {
         headers: { Authorization: `Bearer ${storedToken}` },
       })
       .then((response) => {
-        // Reset the state
         setTitle("");
         setDescription("");
-        navigate("/tournaments")
+        setPrize("");
+        setParticipants("");
+        setSelectedGame(""); // Clear selected game
+        setDateTime("");
+        navigate("/tournaments");
         props.refreshTournaments();
-        
       })
       .catch((error) => console.log(error));
   };
@@ -52,10 +74,44 @@ function AddTournament(props) {
           value={description}
           onChange={(e) => setDescription(e.target.value)}
         />
-
+        <label>Prize:</label>
+        <input
+          type="text"
+          name="prize"
+          value={prize}
+          onChange={(e) => setPrize(e.target.value)}
+        />
+        <label>Participants:</label>
+        <input
+          type="text"
+          name="participants"
+          value={participants}
+          onChange={(e) => setParticipants(e.target.value)}
+        />
+        <label>Game:</label>
+        <select
+          name="game"
+          value={selectedGame}
+          onChange={(e) => setSelectedGame(e.target.value)} // Update selectedGame, not the entire game state
+        >
+          <option value="">Select a game</option>
+          {games.map((game) => (
+            <option key={game._id} value={game._id}>
+              {game.title}
+            </option>
+          ))}
+        </select>
+        <label>Date and time:</label>
+        <input
+          type="datetime-local"
+          name="dateTime"
+          value={dateTime}
+          onChange={(e) => setDateTime(e.target.value)}
+        />
         <button type="submit">Submit</button>
       </form>
     </div>
   );
 }
+
 export default AddTournament;

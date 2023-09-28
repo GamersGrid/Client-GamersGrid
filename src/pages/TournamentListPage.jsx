d103661e481e668efe0b379909f10cc2123df830
Pedro Figueiredo <pedro.figueiredo35@gmail.com>
2023-09-28 16:13:02 +0100

    add loading spinner and logic spinner

--- src/pages/TournamentListPage.jsx
+++ src/pages/TournamentListPage.jsx
@@ -1,40 +1,46 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
 
 import TournamentCard from "../components/TournamentCard";
+import LoadingSpinner from "../components/LoadingSpinner.jsx";
 
+function TournamentListPage() {
+  const [tournaments, setTournaments] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
-function tournamentListPage() {
-    const [tournaments, settournaments] = useState([]);
+  const getAllTournaments = () => {
+    const storedToken = localStorage.getItem("authToken"); 
 
-    const getAlltournaments = () => {
+    axios
+      .get(`${import.meta.env.VITE_API_URL}/api/tournaments`, {
+        headers: { Authorization: `Bearer ${storedToken}` },
+      })
+      .then((response) => {
+        setTournaments(response.data);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        console.log(error);
+        setIsLoading(false);
+      });
+  };
 
-        const storedToken = localStorage.getItem("authToken");
+  useEffect(() => {
+    getAllTournaments();
+  }, []);
 
-        axios
-            .get(
-                `${import.meta.env.VITE_API_URL}/api/tournaments`,
-                { headers: { Authorization: `Bearer ${storedToken}` } }
-            )
-            .then((response) => settournaments(response.data))
-            .catch((error) => console.log(error));
-    };
-
-
-    useEffect(() => {
-        getAlltournaments();
-    }, []);
-
-
-    return (
-        <div className="tournament-list">
-            {tournaments.map((tournament) => (
-                <TournamentCard key={tournament._id} {...tournament} />
-            ))}
-
-        </div>
-    );
+  return (
+    <div className="tournament-list">
+      {isLoading ? (
+        <LoadingSpinner />
+      ) : (
+        tournaments.map((tournament) => (
+          <TournamentCard key={tournament._id} {...tournament} />
+        ))
+      )}
+    </div>
+  );
 }
 
-export default tournamentListPage;
+export default TournamentListPage;
